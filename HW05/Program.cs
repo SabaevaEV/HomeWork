f2d741b48ec04838cff0d8076da16282444d1442
SabaevaEvgeniya <eva230391@mail.ru>
2023-05-30 20:55:01 +0400

    added folders HW06-07 and changes

--- HW05/Program.cs
+++ HW05/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10 Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+int EnterDigit()
+{
+Console.WriteLine("Введите трехзначное число ");
+int x = int.Parse(Console.ReadLine()!);
+return x;
+}
+
+int FirstDigit(int x)
+{
+    int num = x/10;
+    return num;
+}
+ int MiddleDigit (int num)
+ {
+    int arg = num % 10;
+    return arg;
+ }
+
+void FindMiddle (int arg)
+{
+  Console.WriteLine("Вторая цифра введенного Вами числа: " + arg);  
+}
+int x = EnterDigit();
+int num = FirstDigit (x);
+int arg = MiddleDigit (num);
+FindMiddle(arg);
