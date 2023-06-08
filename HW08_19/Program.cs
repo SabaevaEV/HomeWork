b638bd853accf4aabee162fa3ab24fb3aefacbcb
SabaevaEvgeniya <eva230391@mail.ru>
2023-06-09 00:52:09 +0400

    added changes HW08

--- HW08_19/Program.cs
+++ HW08_19/Program.cs
@@ -6,29 +6,38 @@
 int Prompt (string message) // метод ввода данных - 1!
 {
     Console.Write(message);
-    int num = int.Parse(Console.ReadLine()!); // конвертируем данные (для целочисленных данных)
+    int num = Convert.ToInt32(Console.ReadLine()); // конвертируем данные (для целочисленных данных)
+    CheckingValue (num);
     return num;
 }
 
-void FindPolindrom (int num)
+int FindPolindrom (int num)
 {
-    int x = num ;
-    int rev = 0;
-    while (x < )
-    { int r = x % 10;
-      rev = rev*10 + r;
-      x = x / 10;
-      return (rev = num);
+    int n = num ;
+    int rev = 0 ;
+    do { int r = n % 10;
+       rev = rev*10 + r;
+       n = n / 10;}
+    while (n > 0);
+    return rev;
     }
-    if (num = rev)
+
+void CheckingValue (int num)
+{
+    while (num < 10000 || num > 99999)
     {
-        Console.WriteLine ("Число является полиндромом");
-    }
-    else 
-    {
-       Console.WriteLine ("Число не является полиндромом"); 
+    Console.WriteLine ("Введено НЕВЕРНОЕ значение");
+    num = Prompt ("Повторите ввод пятизначного числа: ");
     }
 }
 
 int number = Prompt ("Введите пятизначное число: ");// СМ.выше 1!
-FindPolindrom (number);
+CheckingValue (number);
+if ( FindPolindrom (number) == number)
+    {
+        Console.WriteLine ("Число ЯВЛЯЕТСЯ полиндромом");
+    }
+else 
+    {
+       Console.WriteLine ("Число НЕ ЯВЛЯЕТСЯ полиндромом"); 
+    }
