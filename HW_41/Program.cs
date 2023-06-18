e4873fc3c435b0c2c8fc9bef1f5821cd23f2421f
SabaevaEvgeniya <eva230391@mail.ru>
2023-06-18 20:44:27 +0400

    added hw41-43

---
+++ HW_41/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int[] InPutArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1} элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int PositivNumbers(int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+int length = Prompt("Введите общее количество чисел");
+int[] array = InPutArray(length);
+Console.WriteLine("Введенные числа:");
+PrintArray(array);
+int sum = PositivNumbers(array);
+Console.WriteLine($"Количество чисел больше 0: {sum}");
+
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > 0)
+//         count++;
+// }
+// Console.WriteLine($"Количество чисел больше 0: {count}");
---
+++ HW_43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+
+double b1 = Prompt("Введите значение b1 для прямой заданной уравнением y = k1 * x + b1");
+double k1 = Prompt("Введите значение k1 для прямой заданной уравнением y = k1 * x + b1");
+double b2 = Prompt("Введите значение b2 для прямой заданной уравнением y = k2 * x + b2");
+double k2 = Prompt("Введите значение k2 для прямой заданной уравнением y = k2 * x + b2");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine();
+Console.WriteLine($"Точка пересечения заданных прямых имеет координаты {x};{y}");
